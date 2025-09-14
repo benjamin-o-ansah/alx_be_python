d4b009dea4f294b0e32fa96f34bc5c77db793d44
benjamin-o-ansah <benjaminoansah@gmail.com>
2025-09-14 18:28:00 +0000

    Created a file name finance calculator to calculate the monthly savings and project annual savings assuming an interest rate of 5% per annum

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,14 @@
+#requesting for finanicial details from user
+
+monthly_income = int(input("Enter your monthly income:"))
+total_manual_expenses = int(input("Enter your total monthly expenses:"))
+
+# calculate monthly savings
+monthly_savings = monthly_income - total_manual_expenses
+
+# projecting annual savings
+# assuming an annual interest rate of 5%
+
+projected_annual_savings = (monthly_savings * 12) + (monthly_savings * 12 * 0.05)
+print(f"Your monthly savings are: ${monthly_savings}")
+print(f"Projected savings after one year, with interest, is: ${projected_annual_savings}")
