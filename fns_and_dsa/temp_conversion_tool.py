6306a973eb65be7b894e6a75c7f31ec416b8aaec
benjamin-o-ansah <benjaminoansah@gmail.com>
2025-09-26 18:47:11 +0000

    refixed the global variables

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,14 +1,15 @@
-FAHRENHEIT_TO_CELSIUS_FACTOR = 0
-CELSIUS_TO_FAHRENHEIT_FACTOR = 0
+global FAHRENHEIT_TO_CELSIUS_FACTOR 
+global CELSIUS_TO_FAHRENHEIT_FACTOR 
+FAHRENHEIT_OFFSET = 0
 
 temperature = float(input("Enter the temperature to convert:"))
 temp_type = input("Is this in Celsius or Fahrenheit? (C?F): ").strip().upper()
 def convert_to_celsius(temperature):
-    global FAHRENHEIT_TO_CELSIUS_FACTOR
+    
     FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
     return (temperature - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 def convert_to_fahrenheit(temperature):
-    global CELSIUS_TO_FAHRENHEIT_FACTOR 
+    
     CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
     return (temperature * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 match temp_type:
