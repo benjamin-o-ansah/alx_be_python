620e72e50d0ee14ac3ac16cb36a76f86fed7677b
benjamin-o-ansah <benjaminoansah@gmail.com>
2025-09-26 18:02:16 +0000

    Added a script to get the current date and perform future date calculations

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,18 @@
+#script to tell the current date and time and also tell a future date and time after adding some days
+
+from datetime import datetime, timedelta
+# Get the current date and time
+now = datetime.now()
+print("Current date and time:", now)
+
+def calculate_future_date(days_to_add):
+    # Get user input for number of days to add
+    
+    
+    # Calculate future date
+    future_date = now + timedelta(days=days_to_add)
+    
+    return future_date
+days_to_add = int(input("Enter the number of days to add to the current date: "))
+future_date = calculate_future_date(days_to_add)
+print(f"Future date:", future_date.date())
