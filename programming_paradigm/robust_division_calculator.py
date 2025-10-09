f79bb97b64603e047957f550c9284b26b6033ce9
benjamin-o-ansah <benjaminoansah@gmail.com>
2025-10-09 13:08:21 +0000

    Fixed the typo of the code by introducing a full stop

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -25,6 +25,6 @@ def safe_divide(numerator, denominator):
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
     except ValueError:
-        return "Error: Please enter numeric values only"
+        return "Error: Please enter numeric values only."
     except Exception as e:
         return f"An unexpected error occurred: {e}"
