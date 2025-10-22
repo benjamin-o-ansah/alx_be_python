3e0f917678521573772aee7c7490ad314765670d
benoansah2025 <memphisperla@gmail.com>
2025-10-22 19:43:28 +0000

    created a shape class with a method and created multiple classes inheriting and overiding the area method in the base class

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,18 @@
+class Shape:
+    def area(self):
+        raise NotImplementedError("Subclasses must implement this method")
+    
+class Rectangle(Shape):
+    def __init__(self, width: float, length: float):
+        self.width = width
+        self.length = length
+        
+    def area(self):
+        return self.width * self.length
+    
+class Circle(Shape):
+    def __init__(self, radius: float):
+        self.radius = radius
+        
+    def area(self):
+        return 3.14159 * self.radius ** 2
